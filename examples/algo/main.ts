2aad657430abc9ce2e7301dcc3185ecbba436315
in5net <48165199+in5net@users.noreply.github.com>
2021-09-11 17:07:04 -0400

    pub & sort active

--- examples/algo/main.ts
+++ examples/algo/main.ts
@@ -7,19 +7,23 @@ import { max, shuffle } from '../../src/util';
 
 const randomNumberArr = shuffle(new Array(100).fill(0).map((_, i) => i + 1));
 const m = max(randomNumberArr);
-const iter = sorts.shell(randomNumberArr, (a, b) => a - b);
+const iter = sorts.quick(randomNumberArr, (a, b) => a - b);
+
+let active: number[] = [];
 
 new p5((p: p5) => {
   p.setup = () => {
     p.createCanvas(400, 400);
     p.noStroke();
-    p.fill(0, 0, 255);
+    p.frameRate(10);
   };
 
   p.draw = () => {
     p.background(0);
     const w = p.width / randomNumberArr.length;
     randomNumberArr.forEach((n, i) => {
+      if (active.includes(i)) p.fill(255, 0, 0);
+      else p.fill(255);
       p.rect(
         i * w,
         p.map(n, 0, m, p.height, 0),
@@ -27,6 +31,6 @@ new p5((p: p5) => {
         p.map(n, 0, m, 0, p.height)
       );
     });
-    iter.next();
+    active = iter.next().value || [];
   };
 });
--- src/algorithm/sorts.ts
+++ src/algorithm/sorts.ts
@@ -4,12 +4,12 @@ import { swap } from '../util';
 export function* bubble<T>(
   arr: T[],
   compare: (a: T, b: T) => number
-): Generator<T[]> {
+): Generator<number[]> {
   const { length } = arr;
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < length - 1 - i; j++) {
       if (compare(arr[j]!, arr[j + 1]!) > 0) swap(arr, j, j + 1);
-      yield arr;
+      yield [j, j + 1];
     }
   }
 }
@@ -17,19 +17,19 @@ export function* bubble<T>(
 export function* cocktail<T>(
   arr: T[],
   compare: (a: T, b: T) => number
-): Generator<T[]> {
+): Generator<number[]> {
   const { length } = arr;
   let start = 0;
   let end = length - 1;
   while (start < end) {
     for (let i = start; i < end; i++) {
       if (compare(arr[i]!, arr[i + 1]!) > 0) swap(arr, i, i + 1);
-      yield arr;
+      yield [i, i + 1];
     }
     end--;
     for (let i = end; i > start; i--) {
       if (compare(arr[i]!, arr[i - 1]!) < 0) swap(arr, i, i - 1);
-      yield arr;
+      yield [i, i - 1];
     }
     start++;
   }
@@ -38,29 +38,30 @@ export function* cocktail<T>(
 export function* selection<T>(
   arr: T[],
   compare: (a: T, b: T) => number
-): Generator<T[]> {
+): Generator<number[]> {
   const { length } = arr;
   for (let i = 0; i < length; i++) {
     let min = i;
     for (let j = i + 1; j < length; j++) {
       if (compare(arr[j]!, arr[min]!) < 0) min = j;
+      yield [j];
     }
     if (min !== i) swap(arr, i, min);
-    yield arr;
+    yield [i, min];
   }
 }
 
 export function* insertion<T>(
   arr: T[],
   compare: (a: T, b: T) => number
-): Generator<T[]> {
+): Generator<number[]> {
   const { length } = arr;
   for (let i = 1; i < length; i++) {
     let j = i;
     while (j > 0 && compare(arr[j]!, arr[j - 1]!) < 0) {
       swap(arr, j, j - 1);
       j--;
-      yield arr;
+      yield [j, j - 1];
     }
   }
 }
@@ -68,9 +69,9 @@ export function* insertion<T>(
 export function* quick<T>(
   arr: T[],
   compare: (a: T, b: T) => number
-): Generator<T[]> {
+): Generator<number[]> {
   const { length } = arr;
-  function* sort(left: number, right: number): Generator<T[]> {
+  function* sort(left: number, right: number): Generator<number[]> {
     if (left >= right) return;
     const pivot = arr[left];
     let i = left + 1;
@@ -79,7 +80,7 @@ export function* quick<T>(
       while (i <= right && compare(arr[i]!, pivot!) <= 0) i++;
       while (j > left && compare(arr[j]!, pivot!) > 0) j--;
       if (i < j) swap(arr, i, j);
-      yield arr;
+      yield [left, i, j];
     }
     swap(arr, left, j);
     yield* sort(left, j - 1);
@@ -91,7 +92,7 @@ export function* quick<T>(
 export function* shell<T>(
   arr: T[],
   compare: (a: T, b: T) => number
-): Generator<T[]> {
+): Generator<number[]> {
   const { length } = arr;
   let gap = Math.floor(length / 2);
   while (gap > 0) {
@@ -100,7 +101,7 @@ export function* shell<T>(
       while (j >= gap && compare(arr[j]!, arr[j - gap]!) < 0) {
         swap(arr, j, j - gap);
         j -= gap;
-        yield arr;
+        yield [j, j - gap];
       }
     }
     gap = Math.floor(gap / 2);
@@ -110,9 +111,9 @@ export function* shell<T>(
 export function* merge<T>(
   arr: T[],
   compare: (a: T, b: T) => number
-): Generator<T[]> {
+): Generator<number[]> {
   const { length } = arr;
-  function* mergeSort(left: number, right: number): Generator<T[]> {
+  function* mergeSort(left: number, right: number): Generator<number[]> {
     if (left >= right) return;
     const mid = Math.floor((left + right) / 2);
     yield* mergeSort(left, mid);
@@ -130,23 +131,23 @@ export function* merge<T>(
         j++;
       }
       k++;
-      yield arr;
+      yield [i, j];
     }
     while (i <= mid) {
       temp[k] = arr[i];
       i++;
       k++;
-      yield arr;
+      yield [i];
     }
     while (j <= right) {
       temp[k] = arr[j];
       j++;
       k++;
-      yield arr;
+      yield [j];
     }
     for (let m = 0; m < temp.length; m++) {
       arr[left + m] = temp[m];
-      yield arr;
+      yield [left + m];
     }
   }
   yield* mergeSort(0, length - 1);
@@ -155,9 +156,9 @@ export function* merge<T>(
 export function* heap<T>(
   arr: T[],
   compare: (a: T, b: T) => number
-): Generator<T[]> {
+): Generator<number[]> {
   const { length } = arr;
-  function* heapify(i: number, length: number): Generator<T[]> {
+  function* heapify(i: number, length: number): Generator<number[]> {
     const left = 2 * i + 1;
     const right = 2 * i + 2;
     let largest = i;
@@ -166,7 +167,7 @@ export function* heap<T>(
       largest = right;
     if (largest !== i) {
       swap(arr, i, largest);
-      yield arr;
+      yield [i, largest];
       yield* heapify(largest, length);
     }
   }
@@ -175,7 +176,7 @@ export function* heap<T>(
   }
   for (let i = length - 1; i > 0; i--) {
     swap(arr, 0, i);
-    yield arr;
+    yield [0, i];
     yield* heapify(0, i);
   }
 }
