ca2cf97c10e9c0113d0d045fd7ce6dc2bcbed26f
in5net <48165199+in5net@users.noreply.github.com>
2022-10-14 20:28:29 -0400

    v0.1.1

--- build_npm.ts
+++ build_npm.ts
@@ -2,13 +2,8 @@ import { build, emptyDir } from 'https://deno.land/x/dnt@0.31.0/mod.ts';
 
 await emptyDir('./npm');
 
-const dirs: string[] = [];
-for await (const { name, isDirectory } of Deno.readDir('./src')) {
-  if (isDirectory) dirs.push(name);
-}
-
 await build({
-  entryPoints: ['./src/mod.ts', ...dirs.map(d => `./src/${d}/mod.ts`)],
+  entryPoints: ['./src/mod.ts'],
   outDir: './npm',
   importMap: './import_map.json',
   packageManager: 'bun',
@@ -37,3 +32,16 @@ await build({
 
 Deno.copyFileSync('LICENSE', 'npm/LICENSE');
 Deno.copyFileSync('README.md', 'npm/README.md');
+
+const json = Deno.readTextFileSync('npm/package.json');
+const pkg = JSON.parse(json);
+pkg.exports['./*'] = {
+  import: {
+    default: './esm/*.js',
+    types: './types/*.d.ts'
+  },
+  require: {
+    default: './script/*.js',
+    types: './types/*.d.ts'
+  }
+};
